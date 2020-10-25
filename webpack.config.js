bb198f6a16e04142bf5611ef4649b6aa99f3b22c
Denis Koropalov <249478@gmail.com>
2020-10-25 23:52:03 +0200

    burger menu

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,117 @@
+// const path = require('path');
+// const webpack = require('webpack');
+// const {
+//     CleanWebpackPlugin
+// } = require('clean-webpack-plugin');
+// const HTMLWebpackPlugin = require('html-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//
+//
+// const isProd = process.env.NODE_ENV === 'production';
+// const isDev = !isProd;
+//
+// console.log('is Prod', isProd);
+// console.log('isDev', isDev);
+//
+// const filename = ext => isDev ? `[name].bundle.${ext}` : `[name].bundle.[hash].${ext}`;
+//
+// let htmlPageNames = ['news'];
+//
+// let multipleHtmlPlugins = htmlPageNames.map(name => {
+//     return new HTMLWebpackPlugin({
+//         template: `./pages/${name}/${name}.html`,
+//         filename: `./pages/${name}/${name}.html`,
+//         chunks: [`${name}`]
+//     })
+// });
+//
+// module.exports = {
+//     context: path.resolve(__dirname, 'src'),
+//     mode: 'development',
+//     entry: {
+//         index: './index.js',
+//          news: './pages/news/news.js'
+//     },
+//     output: {
+//         filename: filename('js'),
+//         path: path.resolve(__dirname, 'dist')
+//     },
+//     resolve: {
+//         extensions: ['.js'],
+//         alias: {
+//             '@': path.resolve(__dirname, 'src'),
+//             '@core': path.resolve(__dirname, 'src/core')
+//         }
+//     },
+//     devtool: isDev ? 'source-map' : false,
+//     devServer: {
+//         port: 3000,
+//         hot: isDev
+//     },
+//     plugins: [
+//         new CleanWebpackPlugin(),
+//         new HTMLWebpackPlugin({
+//             template: 'index.html',
+//             minify: {
+//                 removeComments: isProd,
+//                 collapseWhitespace: isProd
+//             },
+//             chunks: ['index']
+//         }),
+//
+//         new CopyPlugin({
+//             patterns: [{
+//                 from: path.resolve(__dirname, 'src/favicon.ico'),
+//                 to: path.resolve(__dirname, 'dist')
+//             },
+//                 {
+//                     from: path.resolve(__dirname, 'src/img'),
+//                     to: path.resolve(__dirname, 'dist/img')
+//                 }
+//             ],
+//         }),
+//         new MiniCssExtractPlugin({
+//             filename: filename('css')
+//         }),
+//         new webpack.DefinePlugin({
+//             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+//         })
+//     //    ],
+//      ].concat(multipleHtmlPlugins),
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.(png|jpg)$/i,
+//                 use: [
+//                     {
+//                         loader: 'image-webpack-loader',
+//                         options: {
+//                             bypassOnDebug: true,
+//                             disable: true,
+//                         },
+//                     },
+//                     'file-loader',
+//                 ],
+//             },
+//             {
+//                 test: /\.s[ac]ss$/i,
+//                 use: [
+//                     {
+//                         loader: MiniCssExtractPlugin.loader,
+//                         options: {
+//                             hmr: isDev,
+//                             reloadAll: true,
+//                         },
+//                     },
+//                     "css-loader",
+//                     "sass-loader",
+//                 ],
+//             },
+//         ],
+//     }
+// };
+
 const path = require('path');
 const webpack = require('webpack');
 const {
@@ -18,6 +132,14 @@ const filename = ext => isDev ? `[name].bundle.${ext}` : `[name].bundle.[hash].$
 
 let htmlPageNames = ['news'];
 
+let getEntry = (pageNames) => {
+    const entry = {};
+    pageNames.forEach(name => {
+        entry[name] = ['@babel/polyfill', `./pages/${name}/${name}.js`]
+    });
+    return entry;
+}
+
 let multipleHtmlPlugins = htmlPageNames.map(name => {
     return new HTMLWebpackPlugin({
         template: `./pages/${name}/${name}.html`,
@@ -26,12 +148,23 @@ let multipleHtmlPlugins = htmlPageNames.map(name => {
     })
 });
 
+const jsLoaders = () => {
+    const loaders = [
+        'babel-loader'
+    ];
+
+    if (isDev) {
+        loaders.push('eslint-loader');
+    }
+    return loaders;
+};
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        index: './index.js',
-         news: './pages/news/news.js'
+        index: ['@babel/polyfill', './index.js'],
+        ...getEntry(htmlPageNames)
     },
     output: {
         filename: filename('js'),
@@ -59,14 +192,6 @@ module.exports = {
             },
             chunks: ['index']
         }),
-/*        new HTMLWebpackPlugin({
-            template: 'pages/news/news.html',
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd
-            },
-            chunks: ['index','news']
-        }),*/
         new CopyPlugin({
             patterns: [{
                 from: path.resolve(__dirname, 'src/favicon.ico'),
@@ -84,8 +209,7 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         })
-    //    ],
-     ].concat(multipleHtmlPlugins),
+    ].concat(multipleHtmlPlugins),
     module: {
         rules: [
             {
@@ -115,6 +239,11 @@ module.exports = {
                     "sass-loader",
                 ],
             },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders()
+            }
         ],
     }
 };
